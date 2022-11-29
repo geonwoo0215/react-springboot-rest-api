927b9c7a616e6faa10a1dd3112cab7e754f4a283
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-11-30 04:26:29 +0900

    feat : ClothingRepository 인터페이스 추가

---
+++ src/main/java/com/example/woodraw/repository/ClothingRepository.java
@@ -0,0 +1,23 @@
+package com.example.woodraw.repository;
+
+import java.util.List;
+
+import com.example.woodraw.domain.clothing.Clothing;
+
+public interface ClothingRepository {
+
+	void save(Clothing clothing);
+
+	Clothing findById(Long id);
+
+	List<Clothing> findAll();
+
+	void updatePriceById(Long id, int price);
+
+	void updateNameById(Long id, String name);
+
+	void deleteById(Long id);
+
+	void deleteAll();
+
+}
