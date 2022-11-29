0d3a57074a701bbda8a9cd1a84c73ca1b1d4b09b
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-11-30 03:53:53 +0900

    initial commit

---
+++ src/main/java/com/example/woodraw/WooDrawApplication.java
@@ -0,0 +1,13 @@
+package com.example.woodraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooDrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/woodraw/WooDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woodraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooDrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
