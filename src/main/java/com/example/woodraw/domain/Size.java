79c95871fe2c3385c7aa713c741766cd5342c107
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-11-30 21:34:50 +0900

    feat : 길이를 통해 사이즈 반환 기능 추가

--- src/main/java/com/example/woodraw/domain/Size.java
+++ src/main/java/com/example/woodraw/domain/Size.java
@@ -1,5 +1,21 @@
 package com.example.woodraw.domain;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public enum Size {
-	SIZE_250
+	SIZE_250("250");
+
+	private final String length;
+
+	Size(String length) {
+		this.length = length;
+	}
+
+	public static Size getSizeByLength(String length) {
+		return Arrays.stream(values())
+			.filter(i -> Objects.equals(i.length, length))
+			.findAny()
+			.orElseThrow(() -> new IllegalArgumentException());
+	}
 }
