a319596db800476d5a7c114ff0095301eaff22d9
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-02 07:01:36 +0900

    feat : productDetailRepository 인터페이스 추가

---
+++ src/main/java/com/example/woodraw/repository/productdetail/ProductDetailRepository.java
@@ -0,0 +1,21 @@
+package com.example.woodraw.repository.productdetail;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.woodraw.domain.product.ProductDetail;
+
+public interface ProductDetailRepository {
+
+	void insert(ProductDetail productDetail);
+
+	Optional<ProductDetail> findById(Long id);
+
+	List<ProductDetail> findAll();
+
+	void updateByObject(ProductDetail productDetail);
+
+	void deleteById(Long id);
+
+	void deleteAll();
+}
