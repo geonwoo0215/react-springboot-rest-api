1dfdbf4099d5f293e4470650fd0333bb73cf8113
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-03 00:58:25 +0900

    test : MemberServiceTest 추가

---
+++ src/test/java/com/example/woodraw/service/member/MemberServiceTest.java
@@ -0,0 +1,118 @@
+package com.example.woodraw.service.member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.woodraw.domain.member.Member;
+import com.example.woodraw.repository.member.MemberJdbcRepository;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+	@Mock
+	MemberJdbcRepository memberJdbcRepository;
+
+	@InjectMocks
+	MemberService memberService;
+
+	@Test
+	@DisplayName("멤버를 저장한다. - 성공")
+	void insertTest() {
+
+		//given
+		Member member = new Member(1L, "이건우", "gw0215");
+		Mockito.doNothing().when(memberJdbcRepository).insert(member);
+
+		//when
+		memberService.insert(member);
+
+		//then
+		Mockito.verify(memberJdbcRepository).insert(member);
+	}
+
+	@Test
+	@DisplayName("멤버를 조회한다. - 성공")
+	void findByIdTest() {
+
+		//given
+		Member member = new Member(1L, "이건우", "gw0215");
+		Mockito.when(memberJdbcRepository.findById(member.getMemberId())).thenReturn(Optional.of(member));
+
+		//when
+		memberService.findById(member.getMemberId());
+
+		//then
+		Mockito.verify(memberJdbcRepository).findById(member.getMemberId());
+	}
+
+	@Test
+	@DisplayName("전체 멤버를 조회한다. - 성공")
+	void findAllTest() {
+
+		//given
+		Member member = new Member(1L, "이건우", "gw0215");
+		List<Member> memberList = new ArrayList<>();
+		memberList.add(member);
+		Mockito.when(memberJdbcRepository.findAll()).thenReturn(memberList);
+
+		//when
+		memberService.findAll();
+
+		//then
+		Mockito.verify(memberJdbcRepository).findAll();
+
+	}
+
+	@Test
+	@DisplayName("멤버를 수정한다. - 성공")
+	void updateByObjectTest() {
+
+		//given
+		Member member = new Member(1L, "이건우", "gw0215");
+		Mockito.doNothing().when(memberJdbcRepository).updateByObject(member);
+
+		//when
+		memberService.updateByObject(member);
+
+		//then
+		Mockito.verify(memberJdbcRepository).updateByObject(member);
+
+	}
+
+	@Test
+	@DisplayName("멤버를 삭제한다. - 성공")
+	void deleteByIdTest() {
+
+		//given
+		Member member = new Member(1L, "이건우", "gw0215");
+		Mockito.doNothing().when(memberJdbcRepository).deleteById(member.getMemberId());
+
+		//when
+		memberService.deleteById(member.getMemberId());
+
+		//then
+		Mockito.verify(memberJdbcRepository).deleteById(member.getMemberId());
+	}
+
+	@Test
+	@DisplayName("전체 멤버를 삭제한다. - 성공")
+	void deleteAll() {
+
+		//given
+		Mockito.doNothing().when(memberJdbcRepository).deleteAll();
+
+		//when
+		memberService.deleteAll();
+
+		Mockito.verify(memberJdbcRepository).deleteAll();
+	}
+}
